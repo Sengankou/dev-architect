07500ab0e50d95659e85cfc1ac244e5c16f34151
Sengankou <ssoqazan@gmail.com>
2025-10-23 16:37:59 +0900

    chore: add biome.json

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  return c.text("Hello Hono! Let's get started!!");
+	return c.text("Hello Hono! Let's get started!!");
 });
 
 export default app;
